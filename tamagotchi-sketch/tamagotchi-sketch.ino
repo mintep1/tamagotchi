69bd117a3188f2fb68822229a3b9afb267632ab1
mintep1 <eminty.me@gmail.com>
2024-04-04 19:57:10 +1300

    finished writing code for buttons and LEDs

--- tamagotchi-sketch/tamagotchi-sketch.ino
+++ tamagotchi-sketch/tamagotchi-sketch.ino
@@ -28,19 +28,29 @@ D)
 */
 
 #include <LiquidCrystal.h>
-#define RedPin 3
-#define GreenPin 6
-#define YellowPin 5
-
-#include <LiquidCrystal.h>
+#define RedPin 4
+#define YellowPin 7
+#define GreenPin 13
+byte noLed = 100;
+byte lastLedState = LOW;
+int redBtn = 8;
+int yellBtn = 9;
+int greenBtn = 10;
+int btnState = 0;
+byte ledArray[4] = { RedPin, GreenPin, YellowPin, noLed };
+unsigned long debounceDuration = 50;  // millis
+unsigned long lastTimeButtonStateChanged = 0;
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 12);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
 
   //set the LED pins to output
+  pinMode(redBtn, INPUT_PULLUP);
+  pinMode(yellBtn, INPUT_PULLUP);
+  pinMode(greenBtn, INPUT_PULLUP);
   pinMode(RedPin, OUTPUT);
   pinMode(GreenPin, OUTPUT);
   pinMode(YellowPin, OUTPUT);
@@ -53,21 +63,60 @@ void turnOff() {
   digitalWrite(RedPin, LOW);
   digitalWrite(GreenPin, LOW);
   digitalWrite(YellowPin, LOW);
+  digitalWrite(greenBtn, 0);
+  digitalWrite(redBtn, 0);
+  digitalWrite(yellBtn, 0);
+}
+
+byte stateCheck(byte val1, byte val2, byte val3) {
+  if (val1 == HIGH) return val1;
+  if (val2 == HIGH) return val2;
+  if (val3 == HIGH) return val3;
+  return LOW;
+}
+
+void lightOff(int led) {
+  digitalWrite(led, LOW);
 }
 
 void loop() {
   //set the cursor to column 0, line 1
   //(note: line 1 is the second row, since counting begins with 0):
   // lcd.setCursor(0, 1);
-  int rand = random(1, 100);
-  Serial.println(rand);
+  //Select random LED to turn on
+  byte led = ledArray[random(0, 4)];
 
-  if (rand <= 25) digitalWrite(RedPin, HIGH);
+  // Read LED states
+  byte red = digitalRead(RedPin);
+  byte green = digitalRead(GreenPin);
+  byte yellow = digitalRead(YellowPin);
+  // Serial.println(red);
+  // Serial.println(yellow);
+  // Serial.println(green);
 
-  if (rand > 25 && rand <= 50) digitalWrite(YellowPin, HIGH);
+  byte anyLedOn = stateCheck(yellow, red, green);
+  if (anyLedOn == LOW) {
+    if (led != noLed) {
+      delay(random(10000, 60000));
+      digitalWrite(led, HIGH);
+    }
+  }
 
-  if (rand > 50) digitalWrite(GreenPin, HIGH);
+  // Read Button States and change LED state
+  byte redState = digitalRead(redBtn);
+  byte greenState = digitalRead(greenBtn);
+  byte yellowState = digitalRead(yellBtn);
 
-  delay(2000);
-  turnOff();
+  if (redState == LOW) {
+    digitalWrite(RedPin, LOW);
+    delay(2000);
+  }
+  if (yellowState == LOW) {
+    digitalWrite(YellowPin, LOW);
+    delay(2000);
+  }
+  if (greenState == LOW) {
+    digitalWrite(GreenPin, LOW);
+    delay(2000);
+  }
 }
